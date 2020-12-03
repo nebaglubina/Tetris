0b4c96fcc898a321253a94ca5ebbc655cea03c20
romanlu <romanlu@playtika.com>
2020-12-03 12:54:51 +0300

    Tests templates added

---
+++ Tetris/Assets/Tests/LinesTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
+
+namespace Tests
+{
+    public class LinesTest : ZenjectUnitTestFixture
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void LinesTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator LinesTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
